6302dd9abf018c906242abc3a5bb2ee39068112e
ArunAvasi <arundavasi@gmail.com>
2023-02-11 17:39:11 -0500

    l

--- Controller.java
+++
@@ -1,9 +0,0 @@
-@Controller
-public class Controller {
-    @GetMapping("/example")
-    public String example(Model model) {
-        model.addAttribute("message", "Hello from the backend!");
-        return "example";
-    }
-}
-
---
+++ src/main/java/newpack/controller/ExController.java
@@ -0,0 +1,43 @@
+package newpack.controller;//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//
+//@Controller
+//public class ExController {
+//
+////    @GetMapping("/")
+////    public String example(Model model) {
+////        model.addAttribute("message", "Hello from the backend!");
+////        return "index";
+////    }
+//
+//    @GetMapping({"/","/hello"})
+//    public String hello(@RequestParam(value="name",defaultValue="World",required=true) String name,Model model)
+//    {
+//        model.addAttribute("name",name);
+//        return "hello";
+//    }
+//
+//}
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ExController {
+
+    @GetMapping({
+            "/",
+            "/hello"
+    })
+    public String hello(@RequestParam(value = "name",
+            defaultValue = "World", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
